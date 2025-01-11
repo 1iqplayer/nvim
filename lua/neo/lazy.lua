e8a6ae4ee1991e6ca8e8dba71ca8a9c91f30876d
neo <neo@example.com>
2025-01-11 18:45:10 +0000

    .

--- lua/neo/lazy.lua
+++ lua/neo/lazy.lua
@@ -23,8 +23,6 @@ require('lazy').setup(
 
 	{'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
 	{'neovim/nvim-lspconfig'},
-	{'hrsh7th/cmp-nvim-lsp'},
-	{'hrsh7th/nvim-cmp'},
 	{'L3MON4D3/LuaSnip'},
 	{'mfussenegger/nvim-dap'},
 	{'rcarriga/nvim-dap-ui'},
@@ -36,5 +34,6 @@ require('lazy').setup(
 	},
 	{'theHamsta/nvim-dap-virtual-text'},
 	{'nvim-neotest/nvim-nio'},
+	{'ms-jpq/coq_nvim'},
     }
     )
--- lua/neo/lsp.lua
+++ lua/neo/lsp.lua
@@ -1,41 +1,3 @@
--- nvim_cmp
-local cmp = require('cmp')
-
-cmp.setup({
-    preselect = 'item',
-    completion = {
-	completeopt = 'menu,menuone,noinsert'
-    },
-    sources = {
-	{ name = 'nvim_lsp' },
-    },
-    mapping = {
-	['<C-u>'] = cmp.mapping.confirm({ select = false }),
-	['<C-e>'] = cmp.mapping.abort(),
-	['<Up>'] = cmp.mapping.select_prev_item({ behavior = 'select' }),
-	['<Down>'] = cmp.mapping.select_next_item({ behavior = 'select' }),
-	['<C-p>'] = cmp.mapping(function()
-	    if cmp.visible() then
-		cmp.select_prev_item({ behavior = 'insert' })
-	    else
-		cmp.complete()
-	    end
-	end),
-	['<C-n>'] = cmp.mapping(function()
-	    if cmp.visible() then
-		cmp.select_next_item({ behavior = 'insert' })
-	    else
-		cmp.complete()
-	    end
-	end),
-    },
-    snippet = {
-	expand = function(args)
-	    require('luasnip').lsp_expand(args.body)
-	end,
-    },
-})
-
 -- lsp_zero
 local lspconfig = require('lspconfig')
 local lsp_zero = require('lsp-zero')
@@ -46,8 +8,11 @@ lsp_zero.on_attach(function(client, bufnr)
 end)
 
 -- here you can setup the language servers
+local coq = require "coq"
 require('mason').setup()
-lspconfig.clangd.setup({})
-lspconfig.lua_ls.setup({})
-lspconfig.omnisharp.setup({cmd = {"/home/neo/.local/share/nvim/mason/packages/omnisharp/omnisharp"}, options = {TabSize = 4, IndentationSize = 4}})
+
+lspconfig.lua_ls.setup()
+lspconfig.lua_ls.setup(coq.lsp_ensure_capabilities())
+lspconfig.pylsp.setup(coq.lsp_ensure_capabilities())
+-- coq config
 
